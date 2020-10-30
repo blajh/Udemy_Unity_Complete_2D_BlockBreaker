2b7b15b4b10980f75cd0b7e0524a516cbbc8545c
Andrej <glazba@gmail.com>
2020-10-30 21:43:12 +0100

    import package from previous project, tidy up

---
+++ Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+	public void LoadNextScene () {
+		int sceneIndex = SceneManager.GetActiveScene().buildIndex;
+		SceneManager.LoadScene(sceneIndex + 1);
+	}
+
+	public void LoadMenuScene() {
+		SceneManager.LoadScene(0); 
+	}
+
+	public void QuitApplication() {
+		Application.Quit();
+	}
+}
