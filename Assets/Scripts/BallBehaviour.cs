55948d9cadb3311c5a8e21d8ebf4b736cc468e79
Andrej <glazba@gmail.com>
2020-10-31 23:29:49 +0100

    launch the ball

--- Assets/Scripts/BallBehaviour.cs
+++ Assets/Scripts/BallBehaviour.cs
@@ -7,22 +7,32 @@ public class BallBehaviour : MonoBehaviour
 {
 	[SerializeField] Paddle paddle1;
 	private Vector2 paddleToBallVector;
+	private bool hasBeenLaunched = false;
 
 	private void Start() {
 		CalculateOffsetToPaddle();
 	}
 
 	private void Update() {
-		FollowPaddle();
+		if (!hasBeenLaunched) {
+			LockBallToPaddle();
+			LaunchOnMouseClick();
+		}
 	}
 
 	private void CalculateOffsetToPaddle() {
 		paddleToBallVector = transform.position - paddle1.transform.position;
 	}
 
-	private void FollowPaddle() {
+	private void LockBallToPaddle() {
 		Vector2 paddlePos = new Vector2(paddle1.transform.position.x, paddle1.transform.position.y);
 		transform.position = paddlePos + paddleToBallVector;
 	}
 	  
+	private void LaunchOnMouseClick() {
+		if (Input.GetMouseButtonDown(0)) {
+			hasBeenLaunched = true;
+			GetComponent<Rigidbody2D>().velocity = new Vector2(2f, 15f);
+		}
+	}
 }
