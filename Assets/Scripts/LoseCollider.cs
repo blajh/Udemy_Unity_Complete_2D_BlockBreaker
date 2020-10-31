13ea8095313f7ba2ba864fb91fea84b3070501b9
Andrej <glazba@gmail.com>
2020-10-31 21:44:07 +0100

    map mouse input to screen width

--- Assets/Scripts/LoseCollider.cs
+++ Assets/Scripts/LoseCollider.cs
@@ -6,7 +6,7 @@ using UnityEngine.SceneManagement;
 public class LoseCollider : MonoBehaviour
 {
 	private void OnTriggerEnter2D(Collider2D collision) {
-		SceneManager.LoadScene("GameOver");
+		SceneManager.LoadScene("GameOver"); 
 	}
 
 }
---
+++ Assets/Scripts/MoveWithMouse.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveWithMouse : MonoBehaviour
+{
+    private float screenWidthToGameplayUnitRatio;
+    private float mousePosition;
+    private Vector3 wantedPosition;
+
+
+    void Update()
+    {
+        UpdatePaddlePosition();
+    }
+
+	public void UpdatePaddlePosition() {
+        screenWidthToGameplayUnitRatio = 11.7f / Screen.width;
+        mousePosition = Input.mousePosition.x * screenWidthToGameplayUnitRatio;
+        wantedPosition = new Vector3(mousePosition, 0f, 0f);
+        transform.SetPositionAndRotation(wantedPosition, transform.rotation);
+    }
+}
