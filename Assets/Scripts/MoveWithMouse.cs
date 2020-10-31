fb3521ff3bc6d4923c560c42e4a4cb2895b648b5
Andrej <glazba@gmail.com>
2020-10-31 21:46:27 +0100

    clamped to game units screen width

--- Assets/Scripts/MoveWithMouse.cs
+++ Assets/Scripts/MoveWithMouse.cs
@@ -6,6 +6,7 @@ public class MoveWithMouse : MonoBehaviour
 {
     private float screenWidthToGameplayUnitRatio;
     private float mousePosition;
+    private float clampedPoition;
     private Vector3 wantedPosition;
 
 
@@ -17,7 +18,8 @@ public class MoveWithMouse : MonoBehaviour
 	public void UpdatePaddlePosition() {
         screenWidthToGameplayUnitRatio = 11.7f / Screen.width;
         mousePosition = Input.mousePosition.x * screenWidthToGameplayUnitRatio;
-        wantedPosition = new Vector3(mousePosition, 0f, 0f);
+        clampedPoition = Mathf.Clamp(mousePosition, 0f, 11.7f);
+        wantedPosition = new Vector3(clampedPoition, 0f, 0f);
         transform.SetPositionAndRotation(wantedPosition, transform.rotation);
     }
 }
