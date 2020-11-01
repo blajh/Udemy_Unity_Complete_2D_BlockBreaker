db821b23304a64035cb9e1fa7d193f8defe3167c
Andrej <glazba@gmail.com>
2020-11-01 01:30:53 +0100

    level win check and transition

---
+++ Assets/Scripts/LevelManager.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelManager : MonoBehaviour
+{
+	private GameObject[] _blocks;
+	private int _blockCount;
+
+	private void Update() {
+		CheckBoxes();
+	}
+
+	private void CheckBoxes() {
+		CountBlocks();
+		if (_blockCount <= 0) {
+			int sceneIndex = SceneManager.GetActiveScene().buildIndex;
+			SceneManager.LoadScene(sceneIndex + 1);
+		}
+	}
+
+	private void CountBlocks() {
+		_blocks = GameObject.FindGameObjectsWithTag("Block");
+		_blockCount = _blocks.Length;
+	}
+}
