c1d2e43544627bf0fa88acc4587a44674d0f00ad
Andrej <glazba@gmail.com>
2020-11-01 12:32:08 +0100

    cleanup

---
+++ Assets/Scripts/GameStatus.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameStatus : MonoBehaviour
+{
+	[SerializeField, Range(0.1f, 10f)]
+	private float gameSpeed = 1f;
+
+	private void Update() {
+		Time.timeScale = gameSpeed;
+	}
+}
